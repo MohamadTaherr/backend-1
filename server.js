3d26e0e40f9c2e9c449aa150c76adb030b774eb8
Mhd102!! <Mohamad7taher@gmail.com>
2025-09-10 18:48:51 +0300

    Added jwt

--- server.js
+++ server.js
@@ -1,3 +1,6 @@
+require("dotenv").config()
+const jwt = require("jsonwebtoken")
+const bcrypt = require("bcrypt")
 const express = require("express")
 const db = require("better-sqlite3")("ourApps.db")
 db.pragma("journal_mode = WAL")
@@ -24,6 +27,17 @@ app.use(express.static("public"))
 
 app.use(function(req, res, next){
     res.locals.errors = []
+
+    //try to decode incoming cookir
+    try{
+      const decoded = jwt.verify(req.cookies.ourSimpleApp, process.env.JWTSECRET)
+      req.user = decoded
+    } catch(err){
+     req.user = false 
+    }
+
+    res.locals.user = req.user 
+    console.log(req.user)
     next()
 })
 
@@ -45,11 +59,11 @@ app.post("/register", (req, res) => {
      req.body.username=req.body.username.trim()
 
      if(!req.body.username) errors.push("You must provide a username")
-     if(req.body.username && req.body.username.length > 3 )  errors.push("Username must be more than 3 characters")
-     if(req.body.username && req.body.username.length < 10 )  errors.push("Username cannot exceed more than 10 characters")
+     if(req.body.username && req.body.username.length < 3 )  errors.push("Username must be more than 3 characters")
+     if(req.body.username && req.body.username.length > 10 )  errors.push("Username cannot exceed more than 10 characters")
 
      if(!req.body.password) errors.push("You must provide a Password")
-     if(req.body.password && req.body.password.length > 8 )  errors.push("Password must be more than 8 characters")
+     if(req.body.password && req.body.password.length < 8 )  errors.push("Password must be more than 8 characters")
       
 
         if (errors.length) {
@@ -58,13 +72,28 @@ app.post("/register", (req, res) => {
         }
 
         //save the new user into database
-       const ourStatement = db.prepare("INSERT INTO users (username, password) VALUES (?, ?)")
-       ourStatement.run(req.body.username, req.body.password) 
+       const salt = bcrypt.genSaltSync(10)
+       req.body.password = bcrypt.hashSync(req.body.password, salt)
       
-       //log the user by giving them a cookie
+        const ourStatement = db.prepare("INSERT INTO users (username, password) VALUES (?, ?)")
+       const result = ourStatement.run(req.body.username, req.body.password) 
+      
+       const lookupStatement = db.prepare("SELECT * FROM users WHERE ROWID = ?")
+       const ourUser = lookupStatement.get(result.lastInsertRowid)    
 
-       res.send("thank you")
-}) 
+       //log the user by giving them a cookie
+       const ourTokenValue = jwt.sign({exp: Math.floor(Date.now() / 1000) + 60 * 60 * 24, skyColor: "blue", userid:ourUser.id, username: ourUser.username}, process.env.JWTSECRET)
+      
+       res.cookie("ourSimpleApp", ourTokenValue, {
+        httpOnly: true,
+        secure: true,
+        sameSite: "strict" ,
+        maxAge: 1000 * 60 * 60 * 24
+       })
+
+       res.send("Thank you")
+
+    }) 
 
 
 app.listen(3000)
