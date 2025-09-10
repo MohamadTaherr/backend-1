879ed40e83956cf0fb7202030973514bf6630a5d
Mhd102!! <Mohamad7taher@gmail.com>
2025-09-10 17:58:39 +0300

    Added sign up errors

--- server.js
+++ server.js
@@ -1,11 +1,30 @@
 const express = require("express")
+const db = require("better-sqlite3")("ourApps.db")
+db.pragma("journal_mode = WAL")
+
+//database
+const createTables = db.transaction(() =>{
+db.prepare(`
+    CREATE TABLE IF NOT EXISTS users (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    username STRING NOT NULL UNIQUE,
+    password STRING NOT NULL
+    )
+    `
+     ).run()
+})
+
+createTables()
+
 const app = express()
 
 app.use(express.urlencoded({extended: false}))
 app.set("view engine", "ejs")
 app.use(express.static("public"))
+
 app.use(function(req, res, next){
     res.locals.errors = []
+    next()
 })
 
 app.get("/", (req, res) => {
@@ -17,7 +36,7 @@ app.get("/login", (req, res) =>{
     res.render("login")
 })
 
-app.post("/register", (req, res) =>{
+app.post("/register", (req, res) => {
     const errors = []
 
     if(typeof req.body.username !=="string") req.body.username=""
@@ -29,20 +48,22 @@ app.post("/register", (req, res) =>{
      if(req.body.username && req.body.username.length > 3 )  errors.push("Username must be more than 3 characters")
      if(req.body.username && req.body.username.length < 10 )  errors.push("Username cannot exceed more than 10 characters")
 
+     if(!req.body.password) errors.push("You must provide a Password")
+     if(req.body.password && req.body.password.length > 8 )  errors.push("Password must be more than 8 characters")
+      
+
         if (errors.length) {
             return res.render("homepage", {errors})
 
-        }else {
-         
-            res.send("thank your for filling out the form")
         }
 
-     
-        
-         
+        //save the new user into database
+       const ourStatement = db.prepare("INSERT INTO users (username, password) VALUES (?, ?)")
+       ourStatement.run(req.body.username, req.body.password) 
+      
+       //log the user by giving them a cookie
 
-
-
+       res.send("thank you")
 }) 
 
 
